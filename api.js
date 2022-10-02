54082584b6c538f743d1a83a2b332466bb8dc3f7
Bappy71 <19103076@iubat.edu>
2022-10-03 03:24:15 +0600

    first commit

--- api.js
+++ api.js
@@ -2,4 +2,20 @@
 //--->> JSON (JAVASCRIPT OBJECT NOTATION)
 
 const user = {id: 11, name: 'HAba Goba', job: 'baker'}
-console.log(user); 
+const stringified =JSON.stringify(user)
+//console.log(user); 
+//console.log(stringified); 
+
+
+const shop = {
+    name : 'KAGU STORE',
+    address: 'hospital road',
+    profit : 20000,
+    products: ['laptop', 'mobile','speaker'],
+    isExpensive: false
+
+}
+
+const shopStringified = JSON.stringify(shop);
+console.log(shop);
+console.log(shopStringified);
