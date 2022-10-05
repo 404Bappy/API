6e31f285761dbd78c7a4502f39d899bc45ffabea
Bappy71 <19103076@iubat.edu>
2022-10-05 06:05:05 +0600

    first commit

--- JS/API.js
+++ JS/API.js
@@ -9,7 +9,7 @@ function loadData() {
 function loadUsers() {
     fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => displayUsers(data));
 }
 
 function loadPost() {
@@ -23,12 +23,12 @@ function loadPost() {
 //}
 
 function displayUsers(data) {
-    const ul = document.getElementById('users')
+    //const ul = document.getElementById('users')
     for (const user of data) {
-        //console.log(user.name);
-        const li = document.getElementById('li');
-        li.innerText='New User';
-        ul.appendChild(li);
+        console.log(user.name);
+        //const li = document.getElementById('li');
+        //li.innerText='New User';
+        //ul.appendChild(li);
 
     }
 }
