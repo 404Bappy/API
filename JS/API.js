fdc575c8ac835eba3fefa2eaebd2e048238f5f6a
Bappy71 <19103076@iubat.edu>
2022-10-05 06:10:03 +0600

    first commit

--- JS/API.js
+++ JS/API.js
@@ -23,12 +23,12 @@ function loadPost() {
 //}
 
 function displayUsers(data) {
-    //const ul = document.getElementById('users')
+    const ul = document.getElementById('users');
     for (const user of data) {
-        console.log(user.name);
-        //const li = document.getElementById('li');
-        //li.innerText='New User';
-        //ul.appendChild(li);
+       // console.log(user.name);
+        const li = document.createElement('li');
+        li.innerText='New User';
+        ul.appendChild(li);
 
     }
 }
