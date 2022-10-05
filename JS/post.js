02007e04034cf64ec03beedecc2bc97709ae8e6b
Bappy71 <19103076@iubat.edu>
2022-10-05 06:42:17 +0600

    first commit

--- JS/post.js
+++ JS/post.js
@@ -10,12 +10,13 @@ function displayPosts(posts){
    const postContainer = document.getElementById('posts');
    for (const post of posts){
        const div = document.createElement('div');
+       div.classList.add('post')
        div.innerHTML = `
        <h3>${post.title}</h3>
        <p>New description </P>
        
        `;
        postContainer.appendChild(div);
-       console.log(post.title);
+       console.log(post);
    }
 }
