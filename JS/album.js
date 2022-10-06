ae9d8a4b21b47bcc02e2a48a9418a167da4758a0
Bappy71 <19103076@iubat.edu>
2022-10-06 06:48:41 +0600

    first commit

--- JS/album.js
+++ JS/album.js
@@ -10,6 +10,8 @@ function displayAlbums(albums){
 
     for( const album of albums){
         const p = document.createElement('p');
+        p.style.textAlign = 'center';
         p.innerText = album.title;
+        albumCintainer.appendChild(p);
     }
 }
