c0e557ded5f740e9d75bc83749a9acbd544e677f
Bappy71 <19103076@iubat.edu>
2022-10-07 06:22:37 +0600

    first commit

---
+++ API Examples/buddy.js
@@ -0,0 +1,12 @@
+const loadBuddies = () => {
+    fetch('https://randomuser.me/api/?results=5')
+    .then(res => res.json())
+    .then(data => displayBuddies(data));
+}
+
+loadBuddies();
+
+const displayBuddies = data => {
+    const buddies = data.results;
+    console.log(data.results);
+}
