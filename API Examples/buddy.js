6f59f5f17843c6ab547afdbd4e91b3aa26bf6f34
Bappy71 <19103076@iubat.edu>
2022-10-08 21:19:58 +0600

    first commit

--- API Examples/buddy.js
+++ API Examples/buddy.js
@@ -12,7 +12,7 @@ const displayBuddies = data => {
     for(const buddy of buddies){
         console.log(buddy.name.first,buddy.name.last);
         const p = document.createElement('p');
-        p.innerText = buddy.email;
+        p.innerText =`Name: ${buddy.name.first} ${buddy.name.last}` buddy.email;
         buddiesDiv.appendChild(p);
     }
 }
