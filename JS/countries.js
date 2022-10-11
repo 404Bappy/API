6e4e3156e4e65b12bcce97519f34008758dea954
Bappy71 <19103076@iubat.edu>
2022-10-11 23:20:26 +0600

    first commit

--- JS/countries.js
+++ JS/countries.js
@@ -1,3 +1,7 @@
 const loadCountries = () =>{
-    .fetch()
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => console.log(data))
 }
+
+loadCountries();
