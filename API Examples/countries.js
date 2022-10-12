f80ff133cefd44cd50e45704550dcaf48fb113c6
Bappy71 <19103076@iubat.edu>
2022-10-12 23:51:49 +0600

    first commit

--- API Examples/countries.js
+++ API Examples/countries.js
@@ -1,14 +1,14 @@
 const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => displayCountries(data))
+        .then(res => res.json())
+        .then(data => displayCountries(data))
 }
 
 loadCountries();
 
 const displayCountries = countries => {
-   // for(const country of countries ){
-   //     console.log(country);
+    // for(const country of countries ){
+    //     console.log(country);
     //}
     const countriesDiv = document.getElementById('countries')
     countries.forEach(country => {
