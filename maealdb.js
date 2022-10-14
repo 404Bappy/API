e5a900da4ad67f9d9fe3f060408e053a536eaad3
Bappy71 <19103076@iubat.edu>
2022-10-15 01:10:43 +0600

    initial  commit

--- maealdb.js
+++ maealdb.js
@@ -24,7 +24,7 @@ const displaySearchResult = meals => {
 
         div.innerHTML = `
         <div class="card h-100">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
