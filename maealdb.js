9407fd71d4ea3b7e31b11e3964826a79ae3c580a
Bappy71 <19103076@iubat.edu>
2022-10-15 00:11:40 +0600

    initial  commit

--- maealdb.js
+++ maealdb.js
@@ -19,5 +19,19 @@ const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal => {
         console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+
+        div.innerHTML = `
+        <div class="card h-100">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        
+        `
     })
 }
