668b0e0eb9682070467ca197da6d57a6dad37fbd
Bappy71 <19103076@iubat.edu>
2022-12-23 08:27:03 +0600

    first commit

---
+++ Browser-API/explore.js
@@ -0,0 +1,9 @@
+const name = 15;
+const student = {
+    name: 'karim', Friend: 'Rabbi', job: "Bussiness",
+    makeMovie: function (name) {
+        console.log(name);
+    }
+};
+student.makeMovie('Din Tne Day');
+document.getElementById('name'); 
