2dc79224e8b6fe1e43ea7b3b276e023a5b921804
Bappy71 <19103076@iubat.edu>
2022-12-24 09:33:17 +0600

    first commit

--- Browser-API/alert.js
+++ Browser-API/alert.js
@@ -5,3 +5,8 @@ alert('Be carefull');
 const maComing = () => {
     alert('ammu is coming. OPen the book');
 }
+
+const askPicnic = () => {
+   const response = confirm('Are you going to Picnic');
+   console.log(response);
+}
